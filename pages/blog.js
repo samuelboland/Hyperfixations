1ec4f603705cb48575b2d8aa88b499b5c727ea78
Sam Boland <sam.boland@appfolio.com>
2022-01-27 21:13:21 -0800

    Use api routes to proxy external request
    Any functions placed in the /pages/api/ directory will be treated
    as serverless functions, and will run only as a lambda once compiled.
    
    This particular function will grab data from an external API for use
    during the compilation step on Vercel. Note that the URL, and any API
    keys that might be used, won't be exposed to a site user.

--- pages/blog.js
+++ pages/blog.js
@@ -1,48 +1,31 @@
+import React from 'react';
 import styles from '../styles/Standard.module.css';
 import ReactMarkdown from 'react-markdown';
-import * as qs from 'qs';
+import { getPosts } from './api/fetchBlogPosts';
 
-function Blog({ posts }) {
-    const STRAPI_API_KEY = process.env.STRAPI_API_KEY;
-    console.log(process.env.STRAPI_API_KEY);
+const Blog = ({ posts }) => {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Posts</h1>
             {posts.map((post) => (
                 <div className={styles.blogPost} key={post.id}>
-                    <ReactMarkdown>{post.attributes.Body}</ReactMarkdown>
-                    <h2 className={styles.postDate}>{post.attributes.Date}</h2>
+                    <h2>{post.title}</h2>
+                    <ReactMarkdown>{post.body}</ReactMarkdown>
+                    <h2 className={styles.postDate}>{post.id}</h2>
                 </div>
             ))}
         </div>
     );
-}
+};
 
-export const getStaticProps = async () => {
-    const myHeaders = new Headers();
-    myHeaders.append(
-        'Authorization',
-        'Bearer 26215454132615f150b4c2299a87a787fca63bb7169cc8b35c08c5c29b914e8b929dac1bd09ad560f388d1158834e0f8d23cd2b804708ddc6ef217ebfffb7602380d06397b802b4e680223970c70f20f69a22d4998c9fd3e71e97c0b2cb1cfad381279e9277abfab37283914cd271003fdca987255f1980f9942ec321b97904f'
-    );
-
-    const requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow',
-    };
-
-    const query = qs.stringify({
-        sort: ['id:desc'],
-    });
-
-    const res = await fetch(`http://127.0.0.1:1337/api/posts?${query}`, requestOptions);
-    const posts = await res.json();
+export async function getStaticProps(context) {
+    const jsonData = await getPosts();
 
     return {
         props: {
-            posts: posts.data,
+            posts: jsonData,
         },
     };
-};
+}
 
 export default Blog;
