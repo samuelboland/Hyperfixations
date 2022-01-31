73ec98e3bc6a0bb33aa371b335c2f0727d631e2f
Sam Boland <sam.boland@appfolio.com>
2022-01-31 15:49:21 -0800

    Do not render header and footer if on new landing page

--- components/layout.jsx
+++ components/layout.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Header from './header';
 import Footer from './footer';
 
+import Router, { useRouter } from 'next/router';
 
 export default function Layout({ children }) {
-    return (
-        <>
-            <Header />
-            {children}
-            <Footer />
-        </>
-    );
+    const router = useRouter();
+    if (router.pathname != '/home') {
+        return (
+            <>
+                <Header />
+                {children}
+                <Footer />
+            </>
+        );
+    } else {
+        return <>{children}</>;
+    }
 }
