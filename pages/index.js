acf91a727ffddc2e63754a435ba63ee9f60e81f8
Sam Boland <sam.boland@appfolio.com>
2022-01-31 10:08:03 -0800

    Adjusting index page styling. Needs more work.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
@@ -6,59 +5,42 @@ export default function Home() {
     return (
         <div>
             <main>
-                <h1>Sam Boland</h1>
-                <h2> Senior QA Engineer @ Appfolio</h2>
-                <h3> I made a website :D</h3>
-
                 <div>
                     <Link href="/about">
                         <a>
                             <h2>About</h2>
-                            <p>
-                                Learn a bit about who I am, what I do, and why I do it. If you want,
-                                I guess.
-                            </p>
                         </a>
                     </Link>
+                    <p>
+                        Learn a bit about who I am, what I do, and why I do it. If you want, I
+                        guess.
+                    </p>
 
                     <Link href="/blog/blogIndex">
                         <a>
                             <h2>Blog </h2>
-                            <p>Sometimes, I think about things and put them on the internet.</p>
                         </a>
                     </Link>
+                    <p>Sometimes, I think about things and put them on the internet.</p>
 
                     <Link href="/">
                         <a>
                             <h2>Todo App </h2>
-                            <p>
-                                Basically the hello, world of react projects at this point. I might
-                                as well do it!
-                            </p>
                         </a>
                     </Link>
+                    <p>
+                        Basically the hello, world of react projects at this point. I might as well
+                        do it!
+                    </p>
 
                     <Link href="/">
                         <a>
                             <h2>Something else</h2>
-                            <p>Lorem Ipsum Dolor Sit etc etc you get it, this is placeholder.</p>
                         </a>
                     </Link>
+                    <p>Lorem Ipsum Dolor Sit etc etc you get it, this is placeholder.</p>
                 </div>
             </main>
-
-            <footer>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{' '}
-                    <span>
-                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                    </span>
-                </a>
-            </footer>
         </div>
     );
 }
