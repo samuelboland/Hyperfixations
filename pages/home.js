a932682237381b114b00a755f87d1d5f234b892c
Sam Boland <sam.boland@appfolio.com>
2022-01-31 15:49:10 -0800

    Testing new modern landing page style

---
+++ pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './home.module.scss';
+
+const Home = () => {
+    return (
+        <div className={styles.gradient}>
+            <h1 className={styles.hometext}>HYPERFIXATION</h1>
+        </div>
+    );
+};
+
+export default Home;
