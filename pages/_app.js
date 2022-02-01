a0005515853f64a4476350ac894b92b93cce6b84
Sam Boland <sam.boland@appfolio.com>
2022-01-31 19:15:02 -0800

    Reintroduce layout component to main _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { SessionProvider } from 'next-auth/react';
-
-import Layout from '../components/layout';
 import 'marx-css';
 import '../styles/globals.scss';
+import { AnimatePresence } from 'framer-motion';
+import Layout from '../components/layout';
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
+export default function App({ Component, pageProps: { session, ...pageProps }, router }) {
     return (
         <SessionProvider session={session}>
             <Layout>
-                <body>
-                    <Component {...pageProps} />
-                </body>
+                <AnimatePresence
+                    exitBeforeEnter
+                    initial="start"
+                    onExitComplete={() => window.scrollTo(0, 0)}
+                >
+                    <Component {...pageProps} key={router.pathname} />
+                </AnimatePresence>
             </Layout>
         </SessionProvider>
     );
