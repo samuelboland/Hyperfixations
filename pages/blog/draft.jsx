86c835f725dac81ed6dece94ec8e21075e0cafb3
Sam Boland <sam.boland@appfolio.com>
2022-02-01 14:03:55 -0800

    Testing page for Tiptap editor

---
+++ pages/blog/draft.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import Tiptap from '../../components/TiptapEdit';
+
+const Draft = () => {
+    return (
+        <div>
+            <Tiptap />
+        </div>
+    );
+};
+
+export default Draft;
