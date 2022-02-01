5d334a5954f95a4e4b7485760724b9476f8d3060
Sam Boland <sam.boland@appfolio.com>
2022-01-31 22:30:36 -0800

    Use the new delete method on the blogIndex page
    Creates a new button that only shows up when logged in. This button
    can be used to delete blog posts. Also, set the page up to use incremental
    re-rendering. Will re-render at most every 10 seconds when a new request
    comes in. If I didn't have this, I would have to rebuild the site each time
    I delete or write a post.

--- pages/blog/blogIndex.js
+++ pages/blog/blogIndex.js
@@ -2,15 +2,49 @@ import React from 'react';
 import AnimationWrapper from '../../components/AnimationWrapper';
 import { getPosts } from '../api/mongoDB_posts';
 import styles from './blog.module.scss';
+import { useSession } from 'next-auth/react';
+import { useState } from 'react';
+import RequireAuth from '../../components/RequireAuth';
 
 const blogIndex = ({ posts }) => {
+    const [message, setMessage] = useState('');
+    const handleDelete = async (e, id) => {
+        e.preventDefault();
+        try {
+            let res = await fetch('/api/mongoDB_posts', {
+                method: 'DELETE',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(id),
+            });
+            if (res.status === 200) {
+                setTitle('');
+                setBody('');
+                setMessage('Post Deleted');
+            } else {
+                setMessage('Something went wrong');
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const session = useSession();
     return (
         <AnimationWrapper>
             <main>
                 {posts.map((post) => (
-                    <div key={post.id}>
+                    <div key={post._id}>
                         <h2 className={styles.post_header}>{post.title}</h2>
                         <p className={styles.post_body}>{post.body}</p>
+                        <RequireAuth>
+                            <button onClick={(e) => handleDelete(e, post._id, posts)}>
+                                Delete
+                            </button>
+                        </RequireAuth>
+                        {message}
                     </div>
                 ))}
             </main>
@@ -21,7 +55,10 @@ const blogIndex = ({ posts }) => {
 export async function getStaticProps(context) {
     const posts = await getPosts();
     return {
-        props: { posts },
+        props: {
+            posts,
+        },
+        revalidate: 10,
     };
 }
 export default blogIndex;
