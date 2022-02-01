db860ecd43fc984293551b839345cfcd4afd1bd4
Sam Boland <sam.boland@appfolio.com>
2022-01-31 19:15:25 -0800

    Wrapp all pages in AnimationWrapper helper

--- pages/blog/blogIndex.js
+++ pages/blog/blogIndex.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import AnimationWrapper from '../../components/AnimationWrapper';
 import { getPosts } from '../api/mongoDB_posts';
 import styles from './blog.module.scss';
 
 const blogIndex = ({ posts }) => {
     return (
-        <main>
-            {posts.map((post) => (
-                <div key={post.id}>
-                    <h2 className={styles.post_header}>{post.title}</h2>
-                    <p className={styles.post_body}>{post.body}</p>
-                </div>
-            ))}
-        </main>
+        <AnimationWrapper>
+            <main>
+                {posts.map((post) => (
+                    <div key={post.id}>
+                        <h2 className={styles.post_header}>{post.title}</h2>
+                        <p className={styles.post_body}>{post.body}</p>
+                    </div>
+                ))}
+            </main>
+        </AnimationWrapper>
     );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,32 @@
 import Link from 'next/link';
 import React from 'react';
+import AnimationWrapper from '../components/AnimationWrapper';
 
 export default function Home() {
     return (
-        <div>
-            <main>
-                <div>
-                    <Link href="/about">
-                        <a>
-                            <h2>About</h2>
-                        </a>
-                    </Link>
-                    <p>
-                        Learn a bit about who I am, what I do, and why I do it. If you want, I
-                        guess.
-                    </p>
+        <AnimationWrapper>
+            <div>
+                <main>
+                    <div>
+                        <Link href="/about">
+                            <a>
+                                <h2>About</h2>
+                            </a>
+                        </Link>
+                        <p>
+                            Learn a bit about who I am, what I do, and why I do it. If you want, I
+                            guess.
+                        </p>
 
-                    <Link href="/blog/blogIndex">
-                        <a>
-                            <h2>Blog </h2>
-                        </a>
-                    </Link>
-                    <p>Sometimes, I think about things and put them on the internet.</p>
-                </div>
-            </main>
-        </div>
+                        <Link href="/blog/blogIndex">
+                            <a>
+                                <h2>Blog </h2>
+                            </a>
+                        </Link>
+                        <p>Sometimes, I think about things and put them on the internet.</p>
+                    </div>
+                </main>
+            </div>
+        </AnimationWrapper>
     );
 }
