b091766c4cf19e32212a030ff6d99f1aa72543cc
Sam Boland <sam.boland@appfolio.com>
2022-01-31 16:58:50 -0800

    Add link and animations to new homepage

--- pages/home.js
+++ pages/home.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import styles from './home.module.scss';
+import 'animate.css';
+import { BsChevronDoubleRight } from 'react-icons/bs';
+import Link from 'next/link';
 
 const Home = () => {
     return (
         <div className={styles.gradient}>
             <h1 className={styles.hometext}>HYPERFIXATION</h1>
+            <h1 className={styles.icon}>
+                <Link href="/">
+                    <a>
+                        <BsChevronDoubleRight className="animate__animated animate__bounceInRight"></BsChevronDoubleRight>
+                    </a>
+                </Link>
+            </h1>
         </div>
     );
 };
