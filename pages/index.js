2594efaf20a67953b19346e14346f9620dc2b60f
Sam Boland <sam.boland@appfolio.com>
2022-02-02 15:43:01 -0800

    Remove link to About on front page
    I don't like how it's set up right now, and I need to think about the
    content more.

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,6 @@ export default function Home() {
             <div>
                 <main>
                     <div>
-                        <Link href="/about">
-                            <a>
-                                <h2>About</h2>
-                            </a>
-                        </Link>
-                        <p>
-                            Learn a bit about who I am, what I do, and why I do it. If you want, I
-                            guess.
-                        </p>
-
                         <Link href="/blog/blogIndex">
                             <a>
                                 <h2>Blog </h2>
