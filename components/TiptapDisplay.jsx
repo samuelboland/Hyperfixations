d0a37506873669fd424ea9c3350de8d171a8448d
Sam Boland <sam.boland@appfolio.com>
2022-02-03 11:18:02 -0800

    Remove a useless variable and directly do config

--- components/TiptapDisplay.jsx
+++ components/TiptapDisplay.jsx
@@ -2,9 +2,8 @@ import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 
 const TiptapDisplay = (props) => {
-    const editable = false;
     const editor = useEditor({
-        editable,
+        editable: false,
         content: props.content,
         extensions: [StarterKit],
     });
