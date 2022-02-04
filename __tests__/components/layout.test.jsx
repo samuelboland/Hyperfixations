42c14949c3547cf4391727f47f8e06f168bca0f5
Sam Boland <sam.boland@appfolio.com>
2022-02-03 21:06:03 -0800

    Add Layout Component Test

---
+++ __tests__/components/layout.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import Layout from '../../components/layout';
+
+/* I have already tested the component that make up my layout.
+ The only new behavior that is introduced here is that it wraps 
+ the rest of the site. Let's test whether that works. */
+
+describe('Layout Component', () => {
+    it('Passes through children', () => {
+        const textToTest = 'This is a list item inside of a div';
+
+        const TestChild = () => {
+            return <li> {textToTest} </li>;
+        };
+
+        render(
+            <Layout>
+                <TestChild />
+            </Layout>,
+        );
+
+        const text = screen.getByRole('listitem', textToTest);
+
+        expect(text).toHaveTextContent(textToTest);
+    });
+});
