d61eca9d37153b8c247c10594631b24411c1ef39
Sam Boland <sam.boland@appfolio.com>
2022-02-04 12:51:16 -0800

    Fix Tiptapedit test

--- __tests__/components/TipTapEdit.test.jsx
+++ __tests__/components/TipTapEdit.test.jsx
@@ -3,17 +3,17 @@ import TiptapEdit from '../../components/TiptapEdit';
 
 describe('Tiptap Editor', () => {
     it('Applies styling to the text that it renders', () => {
-        const testText = '<h1> This is a header </h1> <br /> <li> This is bold text </li>';
+        const testText = '<h1> This is a header </h1> <br /> <li> This is a list item </li>';
 
         render(<TiptapEdit content={testText} />);
-        
+
         const targetHeader = 'This is a header';
-        const targetListItem = 'This is a paragraph';
+        const targetListItem = 'This is a list item';
 
         const header = screen.getByRole('heading', targetHeader);
-        const paragraph = screen.getByRole('listitem', targetListItem);
+        const listitem = screen.getByRole('listitem', targetListItem);
 
         expect(header).toBeInTheDocument;
-        expect(paragraph).toBeInTheDocument;
+        expect(listitem).toBeInTheDocument;
     });
 });
