105d769073adc0559c6275a81a4159c5d20d84a7
Sam Boland <sam.boland@appfolio.com>
2022-02-09 11:37:03 -0800

    Remove Editor component and delete buttons

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -7,52 +7,6 @@ import { AiOutlineDelete } from 'react-icons/ai';
 import styles from './blog.module.scss';
 
 const blogIndex = ({ posts }) => {
-    // My initial implementation of the delete button did not ask for confirmation. I don't want
-    // to accidentally delete my post by clicking once in the wrong place,
-    // so I added this ConfirmationButton component.
-    const ConfirmationButton = (props) => {
-        const [confirm, setConfirm] = useState(false);
-
-        if (confirm === false) {
-            return (
-                <button onClick={(e) => setConfirm(true)}>
-                    <AiOutlineDelete />
-                </button>
-            );
-        } else {
-            return (
-                <button
-                    onClick={(e) => {
-                        handleDelete(e, props.post._id);
-                    }}
-                >
-                    <AiOutlineDelete /> Are you sure?
-                </button>
-            );
-        }
-    };
-
-    const handleDelete = async (e, id) => {
-        e.preventDefault();
-        try {
-            let res = await fetch('/api/mongoDB_posts', {
-                method: 'DELETE',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(id),
-            });
-            if (res.status === 200) {
-                setMessage('Post Deleted');
-            } else {
-                setMessage('Something went wrong');
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
     const DateFormatter = (props) => {
         const event = new Date(props.date);
         const dateString = event.toLocaleDateString();
@@ -69,7 +23,6 @@ const blogIndex = ({ posts }) => {
                             <p>
                                 <DateFormatter date={post.created_at} />
                             </p>
-                            <ConfirmationButton post={post} />
                         </div>
                         <TiptapDisplay content={post.body} />
                     </div>
