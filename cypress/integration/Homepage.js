05db76d905a7cbdf90a11060951468131e457ee8
Sam Boland <sam.boland@appfolio.com>
2022-02-09 23:42:47 -0800

    Update homepage test

--- cypress/integration/Homepage.js
+++ cypress/integration/Homepage.js
@@ -1,23 +1,27 @@
 describe('Navigation', () => {
+    before(() => {
+        cy.visit('/');
+    });
+
+    beforeEach(() => {
+        cy.asAll();
+    });
     // I want to test whether the application loads at all first. It could just use
     // the later tests as a proxy for this one, since obviously they would fail if
     // the app didn't load. However, if a later test fails I'll then have to look
     // into the logs of that test and find out why, and only then will I realize that
     // the app didn't load at all. This
-    it('Loads the application', () => {
-        cy.visit('/');
+    it('Loads the application', () => {});
+
+    it('Has an animation wrapper', () => {
+        cy.get('@animationWrapper');
     });
 
     it('Contains the link to the blog page', () => {
-        cy.get('[data-cy="indexLinkForBlog"]');
+        cy.get('@indexLinkForBlog');
     });
 
     it('Contains the blurb for the blog page', () => {
-        cy.get('[data-cy="indexBlurbForBlog');
-    });
-
-    it('Follows link to blog route', () => {
-        cy.get('[data-cy="indexLinkForBlog"]').click();
-        cy.url().should('include', '/posts');
+        cy.get('@indexBlurbForBlog');
     });
 });
