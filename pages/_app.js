3d6c73367e12792f256b9816c7a6f3b7d48a8581
Sam Boland <sam.boland@appfolio.com>
2022-02-17 09:51:04 -0800

    Add basic SEO to main _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import Head from 'next/head';
 import * as gtag from '../lib/gtag';
 import Script from 'next/script';
 import { useEffect } from 'react';
+import { NextSeo } from 'next-seo';
 
 export default function App({ Component, pageProps: { session, ...pageProps }, router }) {
     useEffect(() => {
@@ -21,6 +22,22 @@ export default function App({ Component, pageProps: { session, ...pageProps }, r
 
     return (
         <>
+            <NextSeo
+                title="Hyperfixations"
+                description="Approximate knowledge of many things"
+                canonical="https://hyperfixations.io/"
+                openGraph={{
+                    url: 'https://hyperfixatons.io/',
+                    title: 'Hyperfixations',
+                    description:
+                        'Follow along as I create and document the process of building a blog with Next.js! Once complete, I will use this to document my various hobby fixations as they come and go.',
+                    site_name: 'Hyperfixations',
+                }}
+                twitter={{
+                    handle: '@SamCBoland',
+                    cardType: 'summary_large_image',
+                }}
+            />
             {/* Global Site Tag (gtag.js) - Google Analytics */}
             <Script
                 strategy="afterInteractive"
@@ -40,12 +57,6 @@ export default function App({ Component, pageProps: { session, ...pageProps }, r
           `,
                 }}
             />
-            <Head>
-                {' '}
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>Sam Boland</title>
-                <meta name="description" content="Sam Bolad's website" />
-            </Head>
             <Layout>
                 <AnimatePresence
                     exitBeforeEnter
