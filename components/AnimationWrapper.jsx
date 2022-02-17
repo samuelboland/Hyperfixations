62b6cb4fc6870f7ac3ec1e0333879ce39090594b
Sam Boland <sam.boland@appfolio.com>
2022-02-16 19:51:57 -0800

    Remove useless styling

--- components/AnimationWrapper.jsx
+++ components/AnimationWrapper.jsx
@@ -14,11 +14,7 @@ const AnimationWrapper = ({ children }) => (
             exit="exit"
             variants={variants}
             transition={{ type: 'linear' }}
-            className="
-                    flex flex-col items-start w-full pt-10
-                    px-8 sm:px-16 md:px-36 lg:px-52 xl:px-80 2xl:px-96
-                    pt-24 h-full
-                "
+
         >
             {children}
         </motion.main>
