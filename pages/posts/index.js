727fe6442d4e456d44a56cb3e0a91f0b38bb757a
Sam Boland <sam.boland@appfolio.com>
2022-02-17 20:08:39 -0800

    Prettify date on posts page, add explainer

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,22 +1,35 @@
 import Link from 'next/link';
 import * as githubApi from '../../lib/githubApi';
+import moment from 'moment';
 
 const index = (props) => {
     const orderPosts = (props) => [...props.data].sort((a, b) => b.date - a.date).reverse();
     const orderedPosts = orderPosts(props);
 
+    const dateFormatter = (date) => {
+        let d = new Date(date);
+        let formatted = moment(d).format('YYYY-MM-DD');
+        return formatted;
+    };
+
     return (
         <div>
+            <main>
+                <h1>Development Log</h1>
+                <p>
+                    A growing record of my progress building the site that you're reading this on.
+                </p>
+            </main>
             {orderedPosts.map((item) => {
                 const title = item.frontmatter.title;
-                const date = item.frontmatter.date;
+                const date = dateFormatter(item.frontmatter.date);
                 const name = item.name.slice(0, -3);
                 const content = item.content;
                 return (
                     <main key={date}>
                         <Link href={'/posts/' + name}>
                             <a data-cy="postIndexLink">
-                                <h1 data-cy="postIndexTitle">{title}</h1>
+                                <h2 data-cy="postIndexTitle">{title}</h2>
                             </a>
                         </Link>
                         <h3 data-cy="postIndexDate">{date}</h3>
