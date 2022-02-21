e97867930e6cd2c24254fa9350b3f35bedaaed46
Sam Boland <sam.boland@appfolio.com>
2022-02-21 14:41:42 -0800

    Remove Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,23 +3,9 @@ import 'marx-css';
 import '../styles/globals.scss';
 import { AnimatePresence } from 'framer-motion';
 import Layout from '../components/layout';
-import Head from 'next/head';
-import * as gtag from '../lib/gtag';
-import Script from 'next/script';
-import { useEffect } from 'react';
 import { NextSeo } from 'next-seo';
 
 export default function App({ Component, pageProps: { session, ...pageProps }, router }) {
-    useEffect(() => {
-        const handleRouteChange = (url) => {
-            gtag.pageview(url);
-        };
-        router.events.on('routeChangeComplete', handleRouteChange);
-        return () => {
-            router.events.off('routeChangeComplete', handleRouteChange);
-        };
-    }, [router.events]);
-
     return (
         <>
             <NextSeo
@@ -36,25 +22,6 @@ export default function App({ Component, pageProps: { session, ...pageProps }, r
                 twitter={{
                     handle: '@SamCBoland',
                     cardType: 'summary_large_image',
-                }}
-            />
-            {/* Global Site Tag (gtag.js) - Google Analytics */}
-            <Script
-                strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-            />
-            <Script
-                id="gtag-init"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
                 }}
             />
             <Layout>
