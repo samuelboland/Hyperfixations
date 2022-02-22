d21e6c83fdae322712bb6565055d4baf6f1c22e3
Sam Boland <sam.boland@appfolio.com>
2022-02-21 17:54:32 -0800

    Create header and footer w/ TailwindCSS & DaisyUI

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,9 +1,25 @@
+import Link from 'next/link';
+import { FaGithub, FaTwitter } from 'react-icons/fa';
+
 const Footer = () => {
     return (
-        <footer data-cy="footer">
-            <main>
-                <p>© Sam Boland, 2022</p>
-            </main>
+        <footer class="footer fixed inset-x-0 bottom-0 items-center bg-neutral p-4 text-neutral-content">
+            <div class="grid-flow-col items-center">
+                <h1>Hyperfixations |</h1>
+                <p>© Sam Boland 2022</p>
+            </div>
+            <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+                <Link href="https://twitter.com/samcboland">
+                    <button class="btn btn-ghost btn-square">
+                        <FaTwitter size={24} />
+                    </button>
+                </Link>
+                <Link href="https://www.github.com/samuelboland/">
+                    <button class="btn btn-ghost btn-square">
+                        <FaGithub size={24} />
+                    </button>
+                </Link>
+            </div>
         </footer>
     );
 };
--- components/header.jsx
+++ components/header.jsx
@@ -1,17 +1,40 @@
+import { FaBars } from 'react-icons/fa';
 import Link from 'next/link';
 
 const Header = () => {
     return (
-        <main>
-            <nav>
+        <div class="navbar bg-base-300">
+            <div class="flex-1">
                 <Link href="/">
-                    <a data-cy="headerLink">
-                        <h1 data-cy="headerText"> Hyperfixations </h1>
-                    </a>
+                    <a class="btn btn-ghost text-xl normal-case">Hyperfixations.io</a>
                 </Link>
-                <div data-cy="headerSubtitle"> Approximate Knowledge of Many Things </div>
-            </nav>
-        </main>
+            </div>
+            <div class="dropdown-left dropdown">
+                <label tabindex="0" class="btn btn-ghost rounded-btn">
+                    <FaBars size={24} />
+                </label>
+                <ul
+                    tabindex="0"
+                    class="dropdown-content menu rounded-box mt-4 w-52 bg-base-100 p-2 shadow"
+                >
+                    <li>
+                        <Link href="/">
+                            <a>Home</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/posts">
+                            <a>Dev Log</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about">
+                            <a>About</a>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
     );
 };
 
