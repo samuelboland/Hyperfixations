e95f22f3075a660040b52c6918d533952ae21cb8
Sam Boland <sam.boland@appfolio.com>
2022-02-23 21:22:23 -0800

    Move about page back to index page, for now

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,103 +1,52 @@
 import Link from 'next/link';
 import React from 'react';
 
-export default function Home() {
+export default function About() {
     return (
-        <section className="body-font text-gray-600">
-            <div className="container mx-auto px-5 py-24">
-                <div className="mb-20 flex w-full flex-wrap">
-                    <div className="mb-6 w-full lg:mb-0 lg:w-1/2">
-                        <h1 className="title-font mb-2 text-2xl font-medium text-gray-900 sm:text-3xl">
-                            Pitchfork Kickstarter Taxidermy
-                        </h1>
-                        <div className="h-1 w-20 rounded bg-red-500"></div>
-                    </div>
-                    <p className="w-full leading-relaxed text-gray-500 lg:w-1/2">
-                        Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify,
-                        subway tile poke farm-to-table. Franzen you probably haven't heard of them
-                        man bun deep jianbing selfies heirloom prism food truck ugh squid celiac
-                        humblebrag.
+        <div className="container prose m-auto">
+            <main>
+                <div>
+                    <h1>Hyperfixations</h1>
+                    <h2> What is this </h2>
+                    <p>
+                        Hello! My name is Sam. I currently work as a Sr. QA Engineer. I am learning
+                        more about the development side of tech work. This site is my classroom, my
+                        playground, and my journal.
+                    </p>
+                    <p>
+                        I intend to document the steps that I take to build this site in the
+                        Development Diary. You can navigate to it with the link below, or with the
+                        menu in the navbar up top.
+                    </p>
+                    <p> This site is built with: </p>
+                    <ul>
+                        <li>Next.JS</li>
+                        <li>Vercel</li>
+                        <li>CircleCI</li>
+                        <li>React</li>
+                        <li>Tailwind CSS</li>
+                        <li>Daisy UI</li>
+                    </ul>
+                    <br />
+                    <p>
+                        This is not an exhaustive list, of course. A more comprehensive list can be
+                        found at the site's repository, here:{' '}
+                        <Link href="https://github.com/samuelboland/Hyperfixations">
+                            <a>Hyperfixations</a>
+                        </Link>
                     </p>
                 </div>
-                <div className="-m-4 flex flex-wrap">
-                    <div className="p-4 md:w-1/2 xl:w-1/4">
-                        <div className="rounded-lg bg-gray-100 p-6">
-                            <img
-                                className="mb-6 h-40 w-full rounded object-cover object-center"
-                                src="https://dummyimage.com/720x400"
-                                alt="content"
-                            />
-                            <h3 className="title-font text-xs font-medium tracking-widest text-red-500">
-                                SUBTITLE
-                            </h3>
-                            <h2 className="title-font mb-4 text-lg font-medium text-gray-900">
-                                Chichen Itza
-                            </h2>
-                            <p className="text-base leading-relaxed">
-                                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-                                hexagon disrupt edison bulbche.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="p-4 md:w-1/2 xl:w-1/4">
-                        <div className="rounded-lg bg-gray-100 p-6">
-                            <img
-                                className="mb-6 h-40 w-full rounded object-cover object-center"
-                                src="https://dummyimage.com/721x401"
-                                alt="content"
-                            />
-                            <h3 className="title-font text-xs font-medium tracking-widest text-red-500">
-                                SUBTITLE
-                            </h3>
-                            <h2 className="title-font mb-4 text-lg font-medium text-gray-900">
-                                Colosseum Roma
-                            </h2>
-                            <p className="text-base leading-relaxed">
-                                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-                                hexagon disrupt edison bulbche.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="p-4 md:w-1/2 xl:w-1/4">
-                        <div className="rounded-lg bg-gray-100 p-6">
-                            <img
-                                className="mb-6 h-40 w-full rounded object-cover object-center"
-                                src="https://dummyimage.com/722x402"
-                                alt="content"
-                            />
-                            <h3 className="title-font text-xs font-medium tracking-widest text-red-500">
-                                SUBTITLE
-                            </h3>
-                            <h2 className="title-font mb-4 text-lg font-medium text-gray-900">
-                                Great Pyramid of Giza
-                            </h2>
-                            <p className="text-base leading-relaxed">
-                                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-                                hexagon disrupt edison bulbche.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="p-4 md:w-1/2 xl:w-1/4">
-                        <div className="rounded-lg bg-gray-100 p-6">
-                            <img
-                                className="mb-6 h-40 w-full rounded object-cover object-center"
-                                src="https://dummyimage.com/723x403"
-                                alt="content"
-                            />
-                            <h3 className="title-font text-xs font-medium tracking-widest text-red-500">
-                                SUBTITLE
-                            </h3>
-                            <h2 className="title-font mb-4 text-lg font-medium text-gray-900">
-                                San Francisco
-                            </h2>
-                            <p className="text-base leading-relaxed">
-                                Fingerstache flexitarian street art 8-bit waistcoat. Distillery
-                                hexagon disrupt edison bulbche.
-                            </p>
-                        </div>
-                    </div>
+                <div>
+                    <Link href="/posts">
+                        <a data-cy="indexLinkForBlog">
+                            <h2 data-cy="indexTextForBlog">Development Diary </h2>
+                        </a>
+                    </Link>
+                    <p data-cy="indexBlurbForBlog">
+                        A (somewhat) real-time record of this site's development
+                    </p>
                 </div>
-            </div>
-        </section>
+            </main>
+        </div>
     );
 }
