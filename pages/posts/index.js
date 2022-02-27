44f602137e3e738ccbeb9dbaa4cc0ebd8951c0b1
Sam Boland <sam.boland@appfolio.com>
2022-02-26 17:15:35 -0800

    Separate PostIndexComponent into own file

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,38 +1,10 @@
-import Link from 'next/link';
-import moment from 'moment';
 import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/router';
 import fs from 'fs';
 import matter from 'gray-matter';
+import PostIndexComponent from '../../components/PostIndexComponent';
 
 const index = ({ posts }) => {
-    const PostComponent = ({ slug, frontmatter }) => {
-        const FormatDate = ({ date }) => {
-            return moment(date).format('dddd, YYYY-MM-DD');
-        };
-        return (
-            <div
-                key={frontmatter.date}
-                className="container mx-auto border-b border-primary px-5 py-5"
-            >
-                <h3 data-cy="postIndexDate">
-                    <FormatDate date={frontmatter.date} />
-                </h3>
-                <Link href={`/posts/${slug}`}>
-                    <a data-cy="postIndexLink">
-                        <h2
-                            data-cy="postIndexTitle"
-                            className="title-font text-base-primary mb-4 text-xl sm:text-2xl"
-                        >
-                            {frontmatter.title}
-                        </h2>
-                    </a>
-                </Link>
-                <h3>{frontmatter.description}</h3>
-            </div>
-        );
-    };
-
     const router = useRouter();
     const canonicalUrl = 'http://hyperfixations.io' + router.asPath;
 
@@ -68,7 +40,7 @@ const index = ({ posts }) => {
                     </div>
                     <div className="mx-auto w-3/4 place-self-center border-b border-primary">
                         {posts.map(({ slug, frontmatter }) => (
-                            <PostComponent slug={slug} frontmatter={frontmatter} />
+                            <PostIndexComponent slug={slug} frontmatter={frontmatter} />
                         ))}
                     </div>
                 </div>
