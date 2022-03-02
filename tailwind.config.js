cc0c8ee1b36f82d48e6eb8aba5b9b06e7134cb72
Sam Boland <sam.boland@appfolio.com>
2022-03-02 10:23:07 -0800

    Restrict available themes in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,24 +5,7 @@ module.exports = {
         './lib/**/*.{js,ts,jsx,tsx}',
     ],
     daisyui: {
-        themes: [
-            'cmyk',
-            {
-                ysayle: {
-                    primary: '#33557d',
-                    secondary: '#60aec6',
-                    accent: '#617091',
-                    neutral: '#18141d',
-                    'base-100': '#e8e7ea',
-                    info: '#18141d',
-                    success: '#45945e',
-                    warning: '#c28426',
-                    error: '#f44336',
-                },
-            },
-            'light',
-            'cupcake',
-        ],
+        themes: ['cmyk'],
     },
     plugins: [require('@tailwindcss/typography'), require('daisyui')],
 };
